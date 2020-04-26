44b2db6e7880b0b56893b4b2e9b23eb7a926cf8d
Sergio Alfaro <sergioalfaro2807@gmail.com>
2020-04-26 17:02:22 -0600

    Form and quote components and localstorage

--- pry-componentes/src/App.js
+++ pry-componentes/src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Form from './components/Form';
+import Quote from './components/Quote';
 
 function App() {
+
+  let initialQuotes = JSON.parse(localStorage.getItem('quotes'));
+  if(!initialQuotes){
+    initialQuotes = [];
+  }
+
+  const [quotes, saveQuotes] = useState(initialQuotes);
+
+
+  useEffect( () => { 
+    if(initialQuotes){
+      localStorage.setItem('quotes', JSON.stringify(quotes));
+    }else{
+      localStorage.setItem('quotes', JSON.stringify([]));
+    }
+  }, [quotes] );
+
+
+  const createQuotes = quote =>{
+      saveQuotes([
+        ...quotes,
+        quote
+      ])
+  }
+
+  const deleteQuote = id => {
+    const newQuotes = quotes.filter(quote => quote.id !== id)
+    saveQuotes(newQuotes);
+  };
+  
+  const titulo = quotes.length === 0 ? "You've no quotes" : 'Quote manangment'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Patient Administration</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form
+             createQuotes={createQuotes}/>
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {quotes.map(quote =>(
+              <Quote
+                key = {quote.id}
+                quote={quote}
+                deleteQuote={deleteQuote}
+              />
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </Fragment>
+   
   );
 }
 
