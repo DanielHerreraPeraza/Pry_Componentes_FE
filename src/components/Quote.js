1fb1383d82ef76a39c09e692b39a0947918a37fe
Sergio Alfaro <sergioalfaro2807@gmail.com>
2020-04-30 21:11:30 -0600

    Fixing time format

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,7 +4,11 @@ const Quote = ({quote, deleteQuote}) => (
     <div className="quote">
         <p>Pet: <span>{quote.pet}</span> </p>
         <p>Owner: <span>{quote.owner}</span> </p>
-        <p>Date: <span>{quote.date}</span> </p>
+        <p>Date: <span>{new Intl.DateTimeFormat("en-GB", {
+          year: "numeric",
+          month: "long",
+          day: "2-digit"
+        }).format(quote.time)}</span> </p>
         <p>Hour: <span>{quote.hour}</span> </p>
         <p>Syntoms: <span>{quote.symptom}</span> </p>
 
