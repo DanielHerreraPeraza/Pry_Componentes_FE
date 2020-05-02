47a1b8c5da177a1aaaacdecec57999740a512207
Daniel José Herrera Peraza <danieljhp@gmail.com>
2020-05-01 18:55:54 -0600

    Add email field to Quote component.  Add email field to Form component.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,6 +4,7 @@ const Quote = ({quote, deleteQuote}) => (
     <div className="quote">
         <p>Pet: <span>{quote.pet}</span> </p>
         <p>Owner: <span>{quote.owner}</span> </p>
+        <p>Email: <span>{quote.email}</span> </p>
         <p>Date: <span>{quote.date}</span> </p>
         <p>Hour: <span>{quote.hour}</span> </p>
         <p>Syntoms: <span>{quote.symptom}</span> </p>
